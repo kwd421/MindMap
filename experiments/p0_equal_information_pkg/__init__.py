ded7760654ff0f3ca0f5eeb613e529ab866c60e4
kwd421 <66898122+kwd421@users.noreply.github.com>
2026-08-17 17:44:40 +0900

    experiment: add P0 package exports

---
+++ experiments/p0_equal_information_pkg/__init__.py
@@ -0,0 +1,7 @@
+"""NCM-Psi equal-information P0 experiment package."""
+
+from .model import *
+from .fixtures import all_fixtures
+from .semantics import *
+from .faults import make_faults
+from .runner import *
