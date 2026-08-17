411c68876cd5cbe1dc9a3953e3b4396ba5d250de
kwd421 <66898122+kwd421@users.noreply.github.com>
2026-08-17 17:54:48 +0900

    fix: export refactored P0 modules

--- experiments/p0_equal_information_pkg/__init__.py
+++ experiments/p0_equal_information_pkg/__init__.py
@@ -2,6 +2,9 @@
 
 from .model import *
 from .fixtures import all_fixtures
-from .semantics import *
+from .validation_common import *
+from .generic_validation import validate_generic_audited
+from .typed_validation import parse_typed
+from .resolvers import *
 from .faults import make_faults
 from .runner import *
